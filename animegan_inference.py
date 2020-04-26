712609383a45ff3c427451fcdfe14456492d6e1d
feng <cy.z.feng@gmail.com>
2020-04-27 00:05:11 +0800

    update: WIP - animagan inference

--- animegan_inference.py
+++ animegan_inference.py
@@ -1,5 +1,26 @@
 import os
+import sys
 import tensorflow as tf
+
+
+def main(argv):
+    default_style_name = ""
+    if len(argv) == 1:
+        default_style_name = "H"
+
+    else:
+        if argv[1] in ["S", "H"]:
+            default_style_name = argv[1]
+        else:
+            raise ValueError(f"{argv[1]} is not a correct style name")
+
+    os.system(
+        f"CUDA_VISIBLE_DEVICES=0 python3 test.py "
+        f"--checkpoint_dir {checkpoint_dir} "
+        f"--test_dir {input_data_dir} "
+        f"--style_name {default_style_name}"
+    )
+
 
 if __name__ == "__main__":
     LOC = os.getcwd()
@@ -13,7 +34,6 @@ if __name__ == "__main__":
     checkpoint_dir = "checkpoint/AnimeGAN_Hayao_lsgan_300_300_1_3_10"
     input_data_dir = os.path.join(LOC, "Data/Input")
     output_data_dir = os.path.join(LOC, "Data/Output")
+    ori_output_data_dir = os.path.join(ANIMEGAN_PREFIX, "Data/Output")
 
-    os.system(
-        f"CUDA_VISIBLE_DEVICES=0 python3 test.py --checkpoint_dir {checkpoint_dir} --test_dir {input_data_dir} --style_name H"
-    )
+    main(sys.argv)
