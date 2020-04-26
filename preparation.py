cd4f8040b3e2f3c16ceb64a74ed0929648000ef1
feng <cy.z.feng@gmail.com>
2020-04-26 22:20:15 +0800

    update: WIP - preparation

--- preparation.py
+++ preparation.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 LOC = os.getcwd()
-ANIMEGAN_PREFIX = os.path.join(LOC, 'AnimeGAN')
-DAIN_PREFIX = os.path.join(LOC, 'DAIN')
+ANIMEGAN_PREFIX = os.path.join(LOC, "AnimeGAN")
+DAIN_PREFIX = os.path.join(LOC, "DAIN")
 
 
 def anime_preparation():
@@ -13,29 +13,49 @@ def anime_preparation():
     pretrain_model_dir = "./checkpoint/AnimeGAN_Hayao_lsgan_300_300_1_3_10"
     vgg_url = "https://github.com/TachibanaYoshino/AnimeGAN/releases/download/vgg16%2F19.npy/vgg19.npy"
     vgg_dir = "./vgg19_weight"
-    vgg_file = os.path.join(vgg_dir, 'vgg19.npy')
+    vgg_file = os.path.join(vgg_dir, "vgg19.npy")
 
-    os.system(f'rm -rf {pretrain_model_dir}')
-    os.system(f'rm -rf {vgg_dir}')
+    os.system(f"rm -rf {pretrain_model_dir}")
+    os.system(f"rm -rf {vgg_dir}")
 
-    os.makedirs(f'{pretrain_model_dir}')
-    os.makedirs(f'{vgg_dir}')
+    os.makedirs(f"{pretrain_model_dir}")
+    os.makedirs(f"{vgg_dir}")
 
-    os.system(f'wget -N {pretrain_model_url} -O {pretrain_model_file}')
-    os.system(f'unzip {pretrain_model_file} -d {pretrain_model_dir}')
-    os.system(f'wget {vgg_url} -O {vgg_file}')
+    os.system(f"wget -N {pretrain_model_url} -O {pretrain_model_file}")
+    os.system(f"unzip {pretrain_model_file} -d {pretrain_model_dir}")
+    os.system(f"wget {vgg_url} -O {vgg_file}")
 
-    os.system(f'rm {pretrain_model_file}')
+    os.system(f"rm {pretrain_model_file}")
 
 
 def dain_preparation():
+    os.chdir(os.path.join(DAIN_PREFIX, "my_package"))
+    os.system(f"sh {os.path.join(DAIN_PREFIX, 'my_package/build.sh')}")
+
+    os.chdir(os.path.join(DAIN_PREFIX, "PWCNet/correlation_package_pytorch1_0"))
+    os.system(
+        f"sh {os.path.join(DAIN_PREFIX, 'PWCNet/correlation_package_pytorch1_0/build.sh')}"
+    )
+
     os.chdir(DAIN_PREFIX)
+    os.system("mkdir model_weights")
+    os.chdir(os.path.join(DAIN_PREFIX, "model_weights"))
+    os.system("wget http://vllab1.ucmerced.edu/~wenbobao/DAIN/best.pth")
+
+    os.system("pip install Pillow scipy==1.1.0")
 
 
 def main(argv):
-    if argv[1] == '01':
-        pass
+    if argv[1] == "animegan":
+        anime_preparation()
+    elif argv[1] == "dain":
+        dain_preparation()
+    elif argv[1] == "all":
+        anime_preparation()
+        dain_preparation()
+    else:
+        raise ValueError("Please select correct function to prepare.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv)
